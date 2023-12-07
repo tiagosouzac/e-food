66063e03f9bcbffd546ffc941cf59e194eb4f25c
Tiago Castro <tiagocastro602@gmail.com>
2023-12-07 20:20:33 -0300

    config(web): initial setup

---
+++ web/app/layout.tsx
@@ -0,0 +1,17 @@
+import "@/globals.css"
+
+export const metadata = {
+  title: "e-Food",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  )
+}
