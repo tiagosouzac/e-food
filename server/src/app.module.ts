13358a38dc7983d32d68804e60dcd8888380ef41
Tiago Castro <tiagocastro602@gmail.com>
2023-12-11 22:41:02 -0300

    config(server): `env`

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { validateEnvVars } from 'env';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.development', '.env.test', '.env'],
+      validate: validateEnvVars,
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
---
+++ server/src/env.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+
+const schema = z.object({});
+
+export const validateEnvVars = (config: Record<string, string>) => {
+  const result = schema.safeParse(config);
+
+  if (!result.success && 'error' in result) {
+    const { errors } = result.error;
+
+    const parsedErrors = errors.reduce((acc, curr) => {
+      acc[curr.path[0]] = curr.message;
+      return acc;
+    }, {});
+
+    throw new Error(JSON.stringify(parsedErrors));
+  }
+
+  return result.data;
+};
