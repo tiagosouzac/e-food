ffad890d6a9ea3e075d2cb695b1f4947066a007b
Tiago Castro <tiagocastro602@gmail.com>
2023-12-11 21:25:05 -0300

    v2

--- server/prisma/migrations/20231209210323_user_optional_updated_at/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `User` MODIFY `updated_at` DATETIME(3) NULL;
--- web/app/(dashboard)/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Restaurant } from "@/components/cards/Restaurant"
-
-export default function Page() {
-  return (
-    <div className="py-6 grid grid-cols-card-list gap-6">
-      <Restaurant />
-    </div>
-  )
-}
